cc73f254bf3c59a0a70e7b0c8f941b9be8e07835
rin-yato <chearithorn@gmail.com>
2023-07-10 23:43:15 +0700

    chore: testing callback route

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -13,5 +13,6 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
+  return NextResponse.json(request);
   return NextResponse.redirect(requestUrl.origin);
 }
